73e849bc067967d24cfae9743aeae3c0b2424cea
Seebyeof <stas.2001070381@gmail.com>
2026-06-24 18:02:57 +0900

    Fix api_yamdb and reviews app

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Category, Genre, Title
+
+from .models import (
+    Category,
+    Genre,
+    Title,
+    Comment,
+    Review,
+    User
+)
 
 
 @admin.register(Category)
@@ -22,3 +30,19 @@ class TitleAdmin(admin.ModelAdmin):
     list_filter = ('category', 'genre')
     search_fields = ('name',)
     filter_horizontal = ('genre',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'score', 'pub_date')
+    list_filter = ('score', 'pub_date')
+    search_fields = ('text', 'author__username', 'title__name')
+    raw_id_fields = ('title', 'author')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'review', 'author', 'pub_date')
+    list_filter = ('pub_date',)
+    search_fields = ('text', 'author__username', 'review__title__name')
+    raw_id_fields = ('review', 'author')
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,75 +4,124 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 
 
+MAX_LENGTH_NAME = 256
+MAX_LENGTH_SLUG = 50
+MIN_SCORE = 1
+MAX_SCORE = 10
+
+
+def current_year():
+    return timezone.now().year
+
+
 User = get_user_model()
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=256, unique=True)
-    slug = models.SlugField(max_length=50, unique=True)
+class BaseNameSlugModel(models.Model):
+    """Абстрактная модель с полями name и slug."""
+    name = models.CharField(
+        max_length=MAX_LENGTH_NAME,
+        unique=True,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=MAX_LENGTH_SLUG,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=256, unique=True)
-    slug = models.SlugField(max_length=50, unique=True)
+class Category(BaseNameSlugModel):
+    class Meta(BaseNameSlugModel.Meta):
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
-    def __str__(self):
-        return self.name
+
+class Genre(BaseNameSlugModel):
+    class Meta(BaseNameSlugModel.Meta):
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
 
 class Title(models.Model):
     name = models.CharField(
-        max_length=256,
-        verbose_name="Название"
+        max_length=MAX_LENGTH_NAME,
+        verbose_name='Название произведения'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
         related_name='titles',
-        verbose_name="Категория"
+        verbose_name='Категория',
+        blank=True,
+        null=True
     )
     genre = models.ManyToManyField(
         Genre,
         related_name='titles',
-        verbose_name="Жанры"
+        verbose_name='Жанры'
     )
-    year = models.PositiveIntegerField(
-        verbose_name="Год выпуска",
-        validators=[MaxValueValidator(limit_value=timezone.now().year)]
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        validators=[MaxValueValidator(limit_value=current_year)],
+        db_index=True
     )
     description = models.TextField(
-        verbose_name="Описание",
+        verbose_name='Описание',
         blank=True
     )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
     def __str__(self):
         return self.name
 
 
-class Review(models.Model):
+class BaseReviewComment(models.Model):
+    """Абстрактная модель с общими полями для отзывов и комментариев."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='%(class)ss',
+        verbose_name='Автор'
+    )
+    text = models.TextField(
+        verbose_name='Текст'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f'{self.author} - {self.text[:20]}...'
+
+
+class Review(BaseReviewComment):
     title = models.ForeignKey(
         Title,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Произведение'
     )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Автор'
-    )
-    text = models.TextField(verbose_name='Текст отзыва')
     score = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        validators=[
+            MinValueValidator(MIN_SCORE),
+            MaxValueValidator(MAX_SCORE)
+        ],
         verbose_name='Оценка'
-    )
-    pub_date = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата публикации'
     )
 
     class Meta:
@@ -83,27 +132,25 @@ class Review(models.Model):
                 name='unique_review'
             )
         ]
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
     def __str__(self):
         return f'{self.author} - {self.title}'
 
 
-class Comment(models.Model):
+class Comment(BaseReviewComment):
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
-        related_name='comments'
+        related_name='comments',
+        verbose_name='Отзыв'
     )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments'
-    )
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
 
     def __str__(self):
         return f'{self.author} - {self.review}'
