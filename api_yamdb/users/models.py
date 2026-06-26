b75f01442ba8ba94a67a9998a54a914028b0619b
Seebyeof <stas.2001070381@gmail.com>
2026-06-26 09:57:59 +0900

    ver 2.0

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,4 +1,3 @@
-import re
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator, EmailValidator
 from django.db import models
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -8,7 +8,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .permissions import IsAdminOrReadOnly, IsAdminOnly
+from .permissions import IsAdminOnly
 from .serializers import (
     SignUpSerializer,
     TokenObtainSerializer,
