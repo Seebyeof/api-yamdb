246d3386e6900658e270693f36a6acb7068ec73b
MauriceBM <125516205+MauriceBM@users.noreply.github.com>
2026-06-28 07:19:48 +0300

    Update constants.py

--- api_yamdb/users/constants.py
+++ api_yamdb/users/constants.py
@@ -1,11 +1,10 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
 USERNAME_MAX_LENGTH = 150
 EMAIL_MAX_LENGTH = 254
 ROLE_MAX_LENGTH = 10
 CONFIRMATION_CODE_MAX_LENGTH = 100
-
-from django.core.validators import RegexValidator
-
-from django.core.exceptions import ValidationError
 
 username_validator = RegexValidator(
     regex=r'^[\w.@+-]+\Z',
